45e8b9333279231d12934d24b3766f3d1e992594
Radu Hociung <radu.csmods@ohmi.org>
2021-04-02 13:45:28 -0500

    Update license headers

--- HarmonyMod/Patches/PluginInfo_userModInstancePatch.cs
+++ HarmonyMod/Patches/PluginInfo_userModInstancePatch.cs
@@ -1,4 +1,21 @@
-﻿extern alias Harmony2;
+﻿/*
+ * Harmony for Cities Skylines
+ *  Copyright (C) 2021 Radu Hociung <radu.csmods@ohmi.org>
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the modified GNU General Public License as
+ *  published in the root directory of the source distribution.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  modified GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+extern alias Harmony2;
 using Harmony2::HarmonyLib;
 using JetBrains.Annotations;
 using System;
