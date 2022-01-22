df81eaccac5abd944d0d24c02596f49f3ba326a4
Radu Hociung <radu.csmods@ohmi.org>
2022-01-22 18:58:58 -0500

    Add Test.ExploitCatch for issue #18
    This test demonstrates reliance on the resolver vulnerability
    reported as issue #18, and how this reliance can break the mod.
    
    The test references VersionedAssembly[1.0.0.0], but ships
    with VersionedAssembly[2.0.0.0], which is not (fully) backwards
    compatible. A feature is removed from 2.0.0.0, which will
    cause the test to fail due to being unable to find the feature it
    expects (which was present at 1.0.0.0, which the test was built
    for).
    
    The resolver offers the only version it has available, 2.0.0.0,
    when 1.0.0.0 is requested, assuming that all assemblies that are
    "similar" are equivalent, regardless of version.
    
    This test demonstrates how the resolver fails, and also how the
    mod fails.

--- Tests.Mod/Test.ReflectionTypeLoadException/ReflectionTypeLoadExceptionTest.cs
+++ Tests.Mod/Test.ReflectionTypeLoadException/ReflectionTypeLoadExceptionTest.cs
@@ -1,4 +1,21 @@
-﻿#if API_1_0_3 || API_1_0_4 || API_1_0_5 || API_1_0_6 || API_2_0_0
+﻿/*
+ * Harmony for Cities Skylines
+ *  Copyright (C) 2021 Radu Hociung <radu.csmods@ohmi.org>
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the modified GNU General Public License as
+ *  published in the root directory of the source distribution.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  modified GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+#if API_1_0_3 || API_1_0_4 || API_1_0_5 || API_1_0_6 || API_2_0_0
 using CitiesHarmony.API;
 #else
 using HarmonyManager;
